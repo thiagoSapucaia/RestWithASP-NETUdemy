327f67f623f8f699f337d0ff2564431e9742aa65
Thiago Sapucaia <thiago.sapucaiaa@gmail.com>
2019-02-05 21:24:02 -0200

    Implementando calculadora REST

---
+++ RestWithASPNETUdemy 01 - Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{fistNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (decimal.TryParse(firstNumber, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out decimal n1) 
+            && decimal.TryParse(secondNumber, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out decimal n2))
+            {
+                var sum = n1 + n2;
+                return this.Ok(sum);
+            }
+            return this.BadRequest("Invalid number format");
+        }
+    }
+}
