63fd5fe1195ebec09b7548dbbaf4479d1af92827
Thiago Sapucaia <thiago.sapucaiaa@gmail.com>
2019-02-05 22:07:23 -0200

    Implementando calculadora REST

--- RestWithASPNETUdemy 01 - Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ RestWithASPNETUdemy 01 - Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace RestWithASPNETUdemy.Controllers
 {
@@ -6,19 +7,78 @@ namespace RestWithASPNETUdemy.Controllers
     [ApiController]
     public class CalculatorController : ControllerBase
     {
-        // GET api/values/5
-        [HttpGet("{fistNumber}/{secondNumber}")]
+        // GET api/calculator/sum/5/5
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if (decimal.TryParse(firstNumber, System.Globalization.NumberStyles.Any,
-                System.Globalization.NumberFormatInfo.InvariantInfo, out decimal n1) 
+                System.Globalization.NumberFormatInfo.InvariantInfo, out decimal n1)
             && decimal.TryParse(secondNumber, System.Globalization.NumberStyles.Any,
                 System.Globalization.NumberFormatInfo.InvariantInfo, out decimal n2))
             {
-                var sum = n1 + n2;
-                return this.Ok(sum);
+                var result = n1 + n2;
+                return this.Ok(result);
             }
             return this.BadRequest("Invalid number format");
         }
+
+        // GET api/calculator/substraction/5/5
+        [HttpGet("substraction/{firstNumber}/{secondNumber}")]
+        public IActionResult Substraction(string firstNumber, string secondNumber)
+        {
+            if (decimal.TryParse(firstNumber, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out decimal n1)
+            && decimal.TryParse(secondNumber, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out decimal n2))
+            {
+                var result = n1 - n2;
+                return this.Ok(result);
+            }
+            return this.BadRequest("Invalid number format");
+        }
+
+        // GET api/calculator/division/5/5
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (decimal.TryParse(firstNumber, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out decimal n1)
+            && decimal.TryParse(secondNumber, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out decimal n2))
+            {
+                var result = n1 / n2;
+                return this.Ok(result);
+            }
+            return this.BadRequest("Invalid number format");
+        }
+
+        // GET api/calculator/average/5/5
+        [HttpGet("average/{firstNumber}/{secondNumber}")]
+        public IActionResult Average(string firstNumber, string secondNumber)
+        {
+            if (decimal.TryParse(firstNumber, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out decimal n1)
+            && decimal.TryParse(secondNumber, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out decimal n2))
+            {
+                var result = (n1 + n2) / 2;
+                return this.Ok(result);
+            }
+            return this.BadRequest("Invalid number format");
+        }
+
+        // GET api/calculator/SquareRoot/5/5
+        [HttpGet("squareroot/{number}")]
+        public IActionResult SquareRoot(string number)
+        {
+            if (double.TryParse(number, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out double n1))
+            {
+                var result = Math.Sqrt(n1);
+                return this.Ok(result);
+            }
+            return this.BadRequest("Invalid number format");
+        }
+
     }
 }
