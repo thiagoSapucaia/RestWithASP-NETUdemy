64420424ba8f9d45b40b51108e02c2986c3b78af
Thiago Sapucaia <thiago.sapucaiaa@gmail.com>
2019-02-07 20:03:01 -0200

    Implementando os 4 principais verbos http

--- RestWithASPNETUdemy 02 - Using Diffents Verbs/RestWithASPNETUdemy/Controllers/PersonsController.cs
+++ RestWithASPNETUdemy 02 - Using Diffents Verbs/RestWithASPNETUdemy/Controllers/PersonsController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Services;
 
 namespace RestWithASPNETUdemy.Controllers
 {
@@ -10,36 +8,57 @@ namespace RestWithASPNETUdemy.Controllers
     [ApiController]
     public class PersonsController : ControllerBase
     {
+        private IPersonService _personService;
+
+        public PersonsController(IPersonService personService)
+        {
+            this._personService = personService;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            return this.Ok(this._personService.FindAll());
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            Person person = this._personService.FindById(id);
+            if (person == null)
+                return this.NotFound();
+            else
+                return this.Ok();
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Person person)
         {
+            if (person == null)
+                return this.BadRequest();
+            else
+                return new ObjectResult(this._personService.Create(person));
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put([FromBody] Person person)
         {
+            if (person == null)
+                return this.BadRequest();
+            else
+                return new ObjectResult(this._personService.Update(person));
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            this._personService.Delete(id);
+            return Ok();
         }
     }
 }
---
+++ RestWithASPNETUdemy 02 - Using Diffents Verbs/RestWithASPNETUdemy/Services/Implementation/PersonService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementation
+{
+    public class PersonService : IPersonService
+    {
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+        }
+
+        public List<Person> FindAll()
+        {
+            return new List<Person>();
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person();
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
